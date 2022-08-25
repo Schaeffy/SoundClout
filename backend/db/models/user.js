20610e19fdd9ff80dd15f098140c3ed7752a1265
Schaeffer <schaefferahn@gmail.com>
2022-08-25 15:34:12 -0700

    Add User signup backend endpoint

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 
 'use strict';
 const {
-  Model
+  Model, Validator
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-     toSafeObject() {
+    toSafeObject() {
       const { id, username, email } = this; // context will be the User instance
       return { id, username, email };
     }
@@ -52,19 +52,19 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.init({
     username: {
-      type:DataTypes.STRING,
-      allowNull:false,
+      type: DataTypes.STRING,
+      allowNull: false,
       validate: {
-        len:[4,30],
-        isNotEmail (value) {
-          if (validator.isEmail(value)) {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
             throw new Error("Cannot be an email")
           }
         }
       }
     },
     email: {
-      type:DataTypes.STRING,
+      type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [3, 256],
@@ -72,10 +72,10 @@ module.exports = (sequelize, DataTypes) => {
       }
     },
     hashedPassword: {
-      type:DataTypes.STRING.BINARY,
+      type: DataTypes.STRING.BINARY,
       allowNull: false,
       validate: {
-        len: [60,60]
+        len: [60, 60]
       }
     },
   }, {
@@ -88,7 +88,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     scopes: {
       currentUser: {
-        attributes: {exclude:["hashedPassword"]}
+        attributes: { exclude: ["hashedPassword"] }
       },
       loginUser: {
         attributes: {}
