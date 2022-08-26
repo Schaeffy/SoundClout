61556b33056ae399003495066d48bf172d45788f
Schaeffer <schaefferahn@gmail.com>
2022-08-26 13:46:01 -0700

    fixing bug

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,13 +10,6 @@ const apiRouter = require('./api');
 router.use('/api', apiRouter);
 // ...
 
-
-// router.get('/hello/world', function(req, res) {
-  //   res.cookie('XSRF-TOKEN', req.csrfToken());
-  //   res.send('Hello World!');
-  // });
-
-  // backend/routes/index.js
   // ...
   // Add a XSRF-TOKEN cookie
   router.get("/api/csrf/restore", (req, res) => {
